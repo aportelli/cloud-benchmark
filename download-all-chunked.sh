b6e5bf1860945b71b32cc3a86f922b71f5f984a2
Antonin Portelli <antonin.portelli@me.com>
2020-11-05 14:52:28 +0000

    checksum check for chunked transfers

--- download-all-chunked.sh
+++ download-all-chunked.sh
@@ -11,11 +11,16 @@ PATH="$(pwd -P)/bin:${PATH}"
 
 echo "-- downloading data from cloud (rclone remote ${REMOTE})"
 ulimit -n 10240 && rclone copy -vv --stats 1000ms --stats-one-line --transfers ${CPUS} "${REMOTE}":lattice-cloud-benchmark/ensemble-chunked download/ensemble-chunked
-#echo '-- verifying checksums'
-#cd download
-#TMP=$(mktemp)
-#cat $(find ensemble -name '*.xxh128') > ${TMP}
-#xxh128sum -c ${TMP}
-#rm -f ${TMP}
-#cd ..
-
+echo '-- recontructing files'
+cd download
+for f in $(find ensemble-chunked -name '*.xxh128'); do
+  STEM=${f//.xxh128}
+  echo ${STEM}
+  cat $(find ensemble-chunked -name "$(basename ${STEM}).*" | grep -v xxh | sort) > ${STEM}
+done
+echo '-- verifying checksums'
+TMP=$(mktemp)
+cat $(find ensemble-chunked -name '*.xxh128') | sed 's/ensemble/ensemble-chunked/g' > ${TMP}
+xxh128sum -c ${TMP}
+rm -f ${TMP}
+cd ..
